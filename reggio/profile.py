748eb8876aa2e1d6e130275f5954ca945f3d4952
iLabSolution <ilaboratorysolution@gmail.com>
2020-09-26 02:51:55 +0300

    add list of individuals in childs profile

--- reggio/profile.py
+++ reggio/profile.py
@@ -2,12 +2,15 @@
 from flask import render_template, url_for, redirect
 
 from reggio import app, db
-from reggio.models import Child
+from reggio.models import individualClass
 from reggio.utils import *
 
 @app.route('/profile/')
 def profile():
     if current_user.is_authenticated:
+        if current_user.userType == "child":
+            individuals = individualClass.query.filter_by(studentUsername=current_user.username)
+            return render_template('profile.html', user=current_user, individuals=individuals, title='profile', menu=defineMenu())
         return render_template('profile.html', user=current_user, title='profile', menu=defineMenu())
     return render_template(
         'main.html',
