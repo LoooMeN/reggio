5962a6edb3ff5baa81060af848ffcafb0dd24a5b
iLabSolution <ilaboratorysolution@gmail.com>
2020-09-25 21:53:21 +0300

    done

--- reggio/utils.py
+++ reggio/utils.py
@@ -5,29 +5,26 @@ from flask_login import current_user
 
 def defineMenu():
     if current_user.is_authenticated:
+        menu = [(u"Главная", "main")]
         if current_user.userType == 'admin':
-            menu = [
-                (u"Главная", "main"),
+            menu.extend([
                 (u"Пользователи", "users"),
                 (u"Уроки", {u"Индивидуалки": "adminIndividualClasses"})
-            ]
+            ])
         elif current_user.userType == 'superAdmin':
-            menu = [
-                (u"Главная", "main"),
+            menu.extend([
                 (u"Пользователи", "users"),
                 (u"Дети", "children"),
                 (u"Учителя", {u"Список": "teachers", u"Индивидуалки": "teachersIndividualClasses"}),
                 (u"Уроки", {u"Индивидуалки": "adminIndividualClasses"}),
                 ("resetDB", "resetDB")
-            ]
+            ])
         elif current_user.userType == 'teacher':
-            menu = [
+            menu.extend([
                 (u"Учителя", {u"Индивидуалки": "individualClasses"}),
-            ]
+            ])
         else :
-            menu = [
-                (u"Главная", "main")
-            ]
+            pass
         menu.append((u'Выйти', 'logout'))
     else:
         menu = [
