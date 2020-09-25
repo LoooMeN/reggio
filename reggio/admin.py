d90a3a97740fb915083bc558f783f90062bfd59d
iLabSolution <ilaboratorysolution@gmail.com>
2020-09-26 00:57:23 +0300

    Create delete individ

--- reggio/admin.py
+++ reggio/admin.py
@@ -5,6 +5,7 @@ from reggio import app, db
 from reggio.models import individualClass
 from reggio.utils import *
 
+
 @app.route('/admin/individualClasses')
 def adminIndividualClasses():
     if not checkPageAvailability(['admin']):
@@ -15,3 +16,14 @@ def adminIndividualClasses():
         individualClasses=individualClasses,
         title=u'Індивідулки',
         menu=defineMenu())
+
+
+@app.route('/admin/deleteIndividualClasses', methods=["GET"])
+def deleteIndividualClasses():
+    if not checkPageAvailability(['admin']):
+        return redirect(url_for('main'))
+    IndividualClassesID = request.args.get('id')
+    IndividualClasses = db.session.merge(individualClass.query.filter_by(id=IndividualClassesID).first())
+    db.session.delete(IndividualClasses)
+    db.session.commit()
+    return redirect(url_for('adminIndividualClasses'))
