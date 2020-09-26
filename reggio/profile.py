5c94c5a6ebe8c6f9d7bc1dd03b13646b1dd8abc5
iLabSolution <ilaboratorysolution@gmail.com>
2020-09-26 17:34:25 +0300

    Change individual list in prifiles, add individuals to other userType, lists was limited by 20 record on page

--- reggio/profile.py
+++ reggio/profile.py
@@ -5,12 +5,29 @@ from reggio import app, db
 from reggio.models import individualClass
 from reggio.utils import *
 
+
+def hasIndividual(userType):
+    has = ["child", "teacher", "admin", "superAdmin", "tutor"]
+    if userType in has:
+        return True
+    else:
+        return False
+
+
+app.jinja_env.globals.update(hasIndividual=hasIndividual)
+app.jinja_env.globals.update(list=list)
+
+
 @app.route('/profile/')
 def profile():
     if current_user.is_authenticated:
-        if current_user.userType == "child":
-            individuals = individualClass.query.filter_by(studentUsername=current_user.username)
-            return render_template('profile.html', user=current_user, individuals=individuals, title='profile', menu=defineMenu())
+        if hasIndividual(current_user.userType):
+            if current_user.userType == "child":
+                individuals = individualClass.query.filter_by(studentUsername=current_user.username).limit(20)
+            else:
+                individuals = individualClass.query.filter_by(teacherUsername=current_user.username).limit(20)
+            return render_template('profile.html', user=current_user, individuals=individuals, title='profile',
+                                   menu=defineMenu())
         return render_template('profile.html', user=current_user, title='profile', menu=defineMenu())
     return render_template(
         'main.html',
