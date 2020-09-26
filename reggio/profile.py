0f6fc2751d81fc7f3fa3a86467f7abf4f8e69ede
kbakhariev <looomen@hotmail.com>
2020-09-26 21:48:25 +0300

    changed profile logic a little bit

--- reggio/profile.py
+++ reggio/profile.py
@@ -7,37 +7,29 @@ from reggio.utils import *
 
 
 def hasIndividual(userType):
-    has = ["child", "teacher", "admin", "superAdmin", "tutor"]
-    if userType in has:
+    legitimateUsersForIndividuals = ["child", "teacher", "admin", "superAdmin", "tutor"]
+    if userType in legitimateUsersForIndividuals:
         return True
     else:
         return False
 
-
-app.jinja_env.globals.update(hasIndividual=hasIndividual)
-app.jinja_env.globals.update(list=list)
-
-
 @app.route('/profile/')
 def profile():
-    if current_user.is_authenticated:
-        if hasIndividual(current_user.userType):
-            if current_user.userType == "child":
-                individuals = individualClass.query.filter_by(studentUsername=current_user.username).limit(20)
-            else:
-                individuals = individualClass.query.filter_by(teacherUsername=current_user.username).limit(20)
-            return render_template(
-                'profile.html',
-                user=current_user,
-                individuals=individuals,
-                title='profile',
-                menu=defineMenu())
+    if not current_user.is_authenticated:
+        return redirect(url_for('main'))
+    if hasIndividual(current_user.userType):
+        if current_user.userType == "child":
+            individuals = individualClass.query.filter_by(studentUsername=current_user.username).limit(20)
+        else:
+            individuals = individualClass.query.filter_by(teacherUsername=current_user.username).limit(20)
         return render_template(
             'profile.html',
             user=current_user,
+            individuals=individuals,
             title='profile',
             menu=defineMenu())
     return render_template(
-        'main.html',
-        title='Main',
+        'profile.html',
+        user=current_user,
+        title='profile',
         menu=defineMenu())
